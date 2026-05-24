60169279375f47dd7ab410796969c24dc23f16a8
Shayla <shaylatyagi@gmail.com>
2026-05-24 14:11:29 +0530

    bottom nav added

--- frontend/src/pages/OwnerDashboard.js
+++ frontend/src/pages/OwnerDashboard.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import api from '../api';
-import BottomNav from '../../components/BottomNav';
+import BottomNav from '../components/BottomNav';
 import DashView from './views/DashView';
 import DispatchView from './views/DispatchView';
 import DriversView from './views/DriversView';
