27b71c3210164f1112db301531af9f9fbd7caaf0
Shayla <shaylatyagi@gmail.com>
2026-05-27 00:45:22 +0530

    bruh

--- frontend/src/pages/OwnerApp.js
+++ frontend/src/pages/OwnerApp.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { 
   Wifi, Battery, Users, Car, Wallet, Plus, CheckCircle,
-  Phone, Home, LogOut, CircleUser, Receipt, ShieldCheck
+  Phone, Home, LogOut, CircleUser, Receipt, ShieldCheck, X
 } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
